154403dfccb58eb6f552f4603ea8f43061a84180
knight <tocaldera6734@gmail.com>
2019-03-24 17:23:06 +0530

    ohhomm

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React now or later
+            Learn React 
           </a>
         </header>
       </div>
